d7972a0122f19b5dbfb0b99ad142de3bf93c2d34
jkywong95 <43147717+jkywong95@users.noreply.github.com>
2019-02-10 19:54:58 -0800

    Updates to the Topology Test

--- TopologySTM32_Slave/TopologySTM32_Slave.ino
+++ TopologySTM32_Slave/TopologySTM32_Slave.ino
@@ -1,11 +1,14 @@
 /***************************************************************************************
 *  Title: Topology Test Slave
 * Author: Jimmy Wong
-* Date: January 21, 2019
-* Code version: 0.0.1
+* Date: February 10, 2019
+* Code version: 0.0.3
 ***************************************************************************************/
 
 #include <Wire_slave.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_DotStarMatrix.h>
+#include <Adafruit_DotStar.h>
 
 // Pin setup
 
@@ -14,15 +17,44 @@
 #define CNCT_L B0100
 #define CNCT_R B1000
 
-#define PIN_DIR_U PB12
-#define PIN_DIR_D PB13
-#define PIN_DIR_L PB14
-#define PIN_DIR_R PB15
+#define PIN_DIR_U PA4
+#define PIN_DIR_D PA6
+#define PIN_DIR_L PA3
+#define PIN_DIR_R PA5
 
 #define LED_U PA3
 #define LED_D PA4
 #define LED_L PA5
 #define LED_R PA2
+
+//DotStar Setup
+const uint8_t MATRIX_DATA_PIN = PB11; 
+const uint8_t MATRIX_CLK_PIN = PB10;
+const uint8_t CHAR_WIDTH = 5;
+const uint8_t CHAR_HEIGHT = 8;
+
+// Size of each tile matrix
+const uint8_t matrixWidth = 4;
+const uint8_t matrixHeight = 4;
+
+const uint8_t tilesX = 1;
+const uint8_t tilesY = 1;
+
+Adafruit_DotStarMatrix matrix = Adafruit_DotStarMatrix(
+  matrixWidth, 
+  matrixHeight, 
+  tilesX, 
+  tilesY,
+  MATRIX_DATA_PIN, 
+  MATRIX_CLK_PIN, 
+  DS_MATRIX_TOP     + DS_MATRIX_LEFT +
+  DS_MATRIX_COLUMNS + DS_MATRIX_ZIGZAG + DS_TILE_PROGRESSIVE,
+  DOTSTAR_RGB
+);
+
+const uint16_t colors[] = {
+  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255), matrix.Color(255, 255, 255) 
+};
 
 int buttonState = 0;
 // I2C 
@@ -33,10 +65,12 @@ int pos_x = 0;
 int pos_y = 0;
 
 int dspy_en = 0;
+int mtrx_en = 0;
 int brightness = 0;
 
 int led_out[2][2] = { {LED_L, LED_U},
                       {LED_D, LED_R}};
+                      
 void handler_tim(void);
 
 void setup()
@@ -46,23 +80,33 @@ void setup()
   Wire.onRequest(requestEvent); // register event
   Wire.onReceive(receiveEvent); // register event
   
-  //Button Setup
+  // Directional Pin Setup
   pinMode(PIN_DIR_U, INPUT_PULLDOWN);
   pinMode(PIN_DIR_D, INPUT_PULLDOWN);
   pinMode(PIN_DIR_L, INPUT_PULLDOWN);
   pinMode(PIN_DIR_R, INPUT_PULLDOWN);
-
+  /*
+  // Directional LED setup **Replace with DotStar** 
   pinMode(LED_U, OUTPUT);
   pinMode(LED_D, OUTPUT);
   pinMode(LED_L, OUTPUT);
-  pinMode(LED_R, OUTPUT);
+  pinMode(LED_R, OUTPUT);*/
 
+  // Timer Setup
   Timer2.setMode(TIMER_CH1, TIMER_OUTPUTCOMPARE);
   Timer2.setPeriod(1000000);
   Timer2.setCompare(TIMER_CH1, 1);
   Timer2.attachInterrupt(TIMER_CH1, handler_tim);
-  
-  Serial.begin(9600);           // start serial for output
+
+  // DotStar Setup - BEGIN 
+  matrix.begin();
+  matrix.setBrightness(64); // Set max brightness 
+  matrix.setTextWrap(false);
+  matrix.setTextColor(colors[0]);
+  matrix.show();  // Turn all LEDs off ASAP
+
+  //  Serial Setup - for output
+  Serial.begin(9600);           
 }
 
 int ports = B0000;
@@ -109,6 +153,25 @@ void loop()
     brightness = (int) msg_buf[2] * 25;
     digitalWrite(led_out[pos_x][pos_y], HIGH);
   }
+  if(mtrx_en){
+    Serial.print("Received: ");
+    Serial.println(msg_buf[0]);
+    matrix.fillScreen(0);
+    if((ports & CNCT_U) == CNCT_U){
+      matrix.fillRect(1, 3, 2, 1, colors[msg_buf[0]]);
+    }
+    if((ports & CNCT_D) == CNCT_D){
+      matrix.fillRect(1, 0, 2, 1, colors[msg_buf[0]]);
+    }
+    if((ports & CNCT_L) == CNCT_L){
+      matrix.fillRect(0, 1, 1, 2, colors[msg_buf[0]]);
+    }
+    if((ports & CNCT_R) == CNCT_R){
+      matrix.fillRect(3, 1, 1, 2, colors[msg_buf[0]]);
+    }    
+    matrix.fillRect( 1, 1, 2, 2, colors[2]);
+    matrix.show();
+  }
 
   
   delay(100);
@@ -143,6 +206,15 @@ void receiveEvent(int howMany)
     Serial.println();
   }else if (c == 'D'){
     dspy_en = 0;
+    mtrx_en = 0;
+  }else if (c == 'B'){
+    mtrx_en = 1;
+        int i = 0;
+    while(Wire.available()){
+      msg_buf[i] = Wire.read();
+      Serial.print(msg_buf[i]);
+      i++;     
+    }
   }
 }
 
